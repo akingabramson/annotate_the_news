585b18bc4527bf8223d95bfc96e438fae7c6c143
Asher King Abramson <a.kingabramson@gmail.com>
2013-06-30 15:28:52 -0700

    can turn snippets into links

--- app/assets/javascripts/views/articles/article_show.js
+++ app/assets/javascripts/views/articles/article_show.js
@@ -10,17 +10,74 @@ NG.Views.ArticleView = Backbone.View.extend({
 		return that
 	},   
 	popupAnnotate: function(event) {
-    var snippetText = '';
-	  if (window.getSelection) {
-	    snippetText = window.getSelection();
-	  }else if(document.getSelection){
-	    snippetText = document.getSelection();
-	  }else if(document.selection){
-	    snippetText = document.selection.createRange().text;
-	  }
+    var that = this;
+    var snippet = that.grabSnippet();
+    if (snippet === '') return;
+    var snippetIndices = that.grabSnippetIndices(snippet);
 
-	  console.log(String(snippetText));
+    // if (that.snippetsOverlap(snippetIndices)) {
+    // 	// fetch the snippet and render view
+    // } else {
+    // 	// popup the annotate link/button
+    // }
+    
+    // if model.snippets
+
+    var bodyText = ($(event.currentTarget).html());
+    var snippetLinkText = JST["articles/snippet_link"]({articleId: that.model.id,
+    																										snippet: snippet});
+    var beginning = bodyText.slice(0, snippetIndices[0]);
+    var end = bodyText.slice(snippetIndices[1], bodyText.length);
+    var renderedsnippet = beginning + snippetLinkText + end;
+    $(event.currentTarget).html(renderedsnippet)
+
+    // stringify html, delete between first and last indexes
+     // put in the template at first index
+     // return html string
+     // put into article-body
+
+    // var link = that.snippetLink(html, snippetIndices[0], snippetIndices[1])
+
+    
+
+
+
   },
+  snippetsOverlap: function(snippetIndices) {
+    this.model.snippets.each(function(snippet){
+    	var range = _.range(snippet.get('start'), snippet.get('end'));
+  		if (_.contains(range, snippetIndices[0]) || _.contains(range, snippetIndices[1])) {
+  		 	return true;
+  		} 
+    });
+    return false;
+  },
+
+  snippetLink: function(originalHTML, start, end) { 
+  	var snippetLink = $("<a>")
+  	snippetLink.addClass("snippet-link");
+  	var text = originalHTML.slice(start, end);
+  	snippetLink.html(text);
+  	return snippetLink
+  },
+
+  grabSnippet: function() {
+		var snippet = '';
+	  if (window.getSelection) {
+	    snippet = window.getSelection();
+	  } else if(document.getSelection){
+	    snippet = document.getSelection();
+	  } else if(document.selection){
+	    snippet = document.selection.createRange().text;
+	  }
+	  return snippet
+  },
+
+  grabSnippetIndices: function(snippet) {
+  	var range = snippet.getRangeAt(0);
+  	var start = range.startOffset, end = range.endOffset;
+  	return [start, end];
+  }
 
 	// snippets, eventHandlers?
 })
